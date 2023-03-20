fdc4a889e2efdd18946f602f7cca74927d2cb769
Filipe Tielson <filipe_thielsom@hotmail.com>
2023-03-20 17:04:11 -0300

    correção

--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -1,18 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
 
-import { Details } from '../pages/Details'
 import { Profile } from '../pages/Profile'
 import { Home } from '../pages/Home'
 import { New } from '../pages/New'
+import { Details } from '../pages/Details'
 
 
 export function AppRoutes() {
   return (
     <Routes>
-      <Route path='/Details/:id' element={<Details />} />
-      <Route path='/Profile' element={<Profile />} />
       <Route path='/' element={<Home />} />
-      <Route path='/New' element={<New />} />
+      <Route path='/details/:id' element={<Details />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/new' element={<New />} />
 
     </Routes>
   )
